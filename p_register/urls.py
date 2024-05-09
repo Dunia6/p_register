4be68f6bce1071cb2566a90ef48523dcd127cde2
Dunia6 <jephtedunia07@gmail.com>
2024-05-09 11:11:54 +0200

    Add students infos v2

--- p_register/urls.py
+++ p_register/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('account.urls')),
     path('', include('etablissement.urls')),
     path('', include('frais.urls')),
